be66ea17c808f1cd56c47030929f1990da71822f
Anubhav Bangari <bangarianubhav@gmail.com>
2025-03-17 15:08:45 +0530

    color changed

--- FrontEnd/src/app/main.tsx
+++ FrontEnd/src/app/main.tsx
@@ -2,14 +2,23 @@ import  {Text, View} from 'react-native';
 
 export default function Main(){
           return(
-              <View className="flex-row bg-white p-4 shadow-lg justify-around items-center border-b border-gray-300">
-                     <Text className="text-lg font-semibold text-gray-700">Niyam</Text>
-                     <Text className="text-lg font-semibold text-gray-700">Blogs</Text>
-                     <Text className="text-lg font-semibold text-gray-700">Audiobooks</Text>
-                     <Text className="text-lg font-semibold text-gray-700">Meditations</Text>
-                     <Text className="text-lg font-semibold text-gray-700">Films</Text>
-                     <Text className="text-lg font-semibold text-gray-700">E-Books</Text>
+
+              <View>
+
+                     <View className="flex-row bg-yellow-300 p-4 shadow-lg justify-around items-center border-b border-gray-300">
+                            <Text className="text-lg font-semibold text-white">NIYAM</Text>
+                            <Text className="text-lg font-semibold text-white">Blogs</Text>
+                            <Text className="text-lg font-semibold text-white">Audiobooks</Text>
+                            <Text className="text-lg font-semibold text-white">Meditations</Text>
+                            <Text className="text-lg font-semibold text-white">Films</Text>
+                            <Text className="text-lg font-semibold text-white">E-Books</Text>
+                     </View>
+                     <View>
+                            Hello
+                     </View>
+
               </View>
+             
             
 
                 
